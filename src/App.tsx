b3cb9905099f49c615b04dc67dd1e475f57fe174
Siddhant Jain <codebysidd@gmail.com>
2024-12-29 23:41:33 +0530

    glow fixes

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import ZeroCost from "./components/ZeroCost";
 
 function App() {
   return (
-    <div className="dark pt-32 min-h-screen max-w-screen bg-background text-foreground flex flex-col justify-center items-center gap-10 after:absolute after:h-[70%] after:w-[85%] after:rounded-full after:bg-primary after:-top-[500px] after:shadow-[0px_150px_1000px_50px_rgba(127,0,255,0.2)] grid-background">
+    <div className="dark pt-32 min-h-screen max-w-screen bg-background text-foreground flex flex-col justify-center items-center gap-10 after:absolute after:h-[70%] after:w-[85%] after:rounded-full after:bg-primary after:-top-full after:shadow-[0px_350px_1000px_50px_rgba(127,0,255,0.2)] grid-background">
       <Logo />
       <Hero />
       <DemoVideo />
